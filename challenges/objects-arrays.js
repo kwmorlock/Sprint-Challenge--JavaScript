a5a1a50607d8c440f4489b834be61356bc22a18d
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-02-28 12:01:51 -0500

    dinos done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,39 +7,54 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-const tyrannosaurus = {
+const tyrannosaurus1 = {
   //used const instead of let, because this dino should not change, var is only used when truly needed
   name: 'tyrannosaurus',
   diet: 'carnivorous',
   weight: '7000kg',
   length: '12m',
   period: 'Late Cretaceous',
-
 }
+tyrannosaurus1.roar = function() {
+  return 'RAWERSRARARWERSARARARRRR!'}
 
 
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus1 = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor1 = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log(tyrannosaurus.weight);
+console.log(tyrannosaurus1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor1.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus1.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus1.roar);
 
 
 // ==== Arrays ====
